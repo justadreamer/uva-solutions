65b036c98b8a7582dd4c9af1e5c40a5d0c4e8ae2
Eugene Dorfman <eugene@Eugenes-MacBook-Pro.local>
2013-06-04 22:35:31 +0300

    added new.sh script to start a problem

---
+++ new.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+dir=uva$1
+mkdir $dir
+./download.py $1
+
+cp template/main.cpp $dir/$dir.cpp
+echo "paste input.txt, then press Enter, ^D"
+cat > $dir/input.txt
+echo "paste output.txt, then press Enter, ^D"
+cat > $dir/output.txt
+subl $dir/$dir.cpp
