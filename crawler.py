2bba1507212892d30b3b298362189c74c09d136d
Eugene Dorfman <eugene@Eugenes-MacBook-Pro.local>
2013-06-22 22:28:52 +0300

    added crawler.py and open.py scripts, with the help of first we scraped urls for all the existing problems in UVA OJ, with the help of open.py we now are able to open the given problem in the browser by just supplying its number, both require mysqld to be running. open.py requires to first run crawler.py and populate db with data.

---
+++ crawler.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+import urllib2
+import json
+from multiprocessing import Pool, Queue
+import mysql.connector
+from bs4 import BeautifulSoup
+import string
+import ConfigParser
+
+'''
+CREATE TABLE `problems` (
+  `problem_id` varchar(5) NOT NULL,
+  `problem_title` varchar(256) DEFAULT NULL,
+  `problem_url` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`problem_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+'''
+
+config = ConfigParser.ConfigParser()
+config.read('db.cfg')
+cfg = dict((k,v) for k,v in config.items('mysql'))
+
+base_url = 'http://uva.onlinejudge.org/'
+
+
+def parse_volumes(url):
+    resp = urllib2.urlopen(url)
+    soup = BeautifulSoup(resp.read())
+    div = soup.find('div',id='col3_content_wrapper')
+    table = div.find('table')
+    links = table.find_all('a')
+    return map(lambda link: link['href'],links)
+
+def parse_problems(url):
+    resp = urllib2.urlopen(base_url+url)
+    soup = BeautifulSoup(resp.read())
+    resp.close()
+    div = soup.find('div',id='col3_content_wrapper')
+    table = div.find('table')
+    links = table.find_all('a')
+
+    con = mysql.connector.connect(
+        host=cfg['db_host'],
+        user=cfg['db_user'],
+        passwd=cfg['db_passwd'],
+        db=cfg['db_name']
+        )
+    cur = con.cursor()
+
+    query_insert = ("INSERT IGNORE INTO problems "
+              "(problem_id, problem_title, problem_url) "
+              "VALUES (%s, %s, %s) ")
+    for link in links:
+        problem_title = link.string.encode('ascii','ignore')
+        problem_url = base_url+link['href'].encode('ascii','ignore')
+        problem_id = string.split(problem_title,'-')[0]
+        problem_id = '{0:05d}'.format(int(problem_id)).encode('ascii','ignore')
+        print problem_id,problem_title,problem_url
+        cur.execute(query_insert,(problem_id,problem_title,problem_url))
+        con.commit()
+    cur.close()
+    con.close()
+
+
+if __name__ == '__main__':
+    top_volumes=[
+        base_url+'index.php?option=com_onlinejudge&Itemid=8&category=1',
+        base_url+'index.php?option=com_onlinejudge&Itemid=8&category=2'
+    ]
+    links = []
+    for volume in top_volumes:
+        links.extend(parse_volumes(volume))
+
+    for link in links:
+        parse_problems(link)
---
+++ open.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys
+import os
+import urllib2
+import mysql.connector
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+config.read('db.cfg')
+cfg = dict((k,v) for k,v in config.items('mysql'))
+
+def open_problem(problem_id):
+	con = mysql.connector.connect(
+		host=cfg['db_host'],
+		user=cfg['db_user'],
+		passwd=cfg['db_passwd'],
+		db=cfg['db_name']
+		)
+	cur = con.cursor()
+	cur.execute('select problem_url from problems where problem_id=%s',[problem_id])
+	url = cur.fetchone()
+	print url
+	os.system('open '+'"'+url[0]+'"')
+
+open_problem('00159')
+
+if len(sys.argv)<2:
+	print "usage: open.py {5_digit_uva_problem_number}"
+else:
+	for arg in sys.argv[1:]:
+		open_problem(arg)
