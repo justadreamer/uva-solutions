5a04ede4d315372fc6dfeff1d4375c7f9d3fdce1
Eugene Dorfman <eugene@Eugenes-MacBook-Pro.local>
2013-06-22 15:51:40 +0300

    replaced an ad hoc solution to uva00599 (The Forrest for the Trees), with the graph-property solution (joined components = v - e) - this way it is simpler (less code) and more efficient

--- uva00599_ad_hoc/uva00599.cpp
+++ uva00599_ad_hoc/uva00599.cpp
@@ -9,7 +9,6 @@
 #include <list>
 #include <stack>
 #include <queue>
-#include <bitset>
 
 #include <cstdio>
 #include <cmath>
@@ -39,38 +38,57 @@ private:
 	void input() {
 		char buf[512];
 		buf[0]='\0';
-		e = 0;
 		while (scanf("%s",buf) && buf[0]!='*') {
-			++e;
-			char v1 = buf[1]-'A';
-			char v2 = buf[3]-'A';
-			ve[v1]=true;
-			ve[v2]=true;
+			char v1 = buf[1];
+			char v2 = buf[3];
+			g[v1].adj_list.push_back(v2);
+			g[v2].adj_list.push_back(v1);
 		}
 		scanf("%s",buf);
 		int i=0;
-		v = 0;
 		while (buf[i]!='\0') {
-			char c = buf[i];
-			if (isalpha(c)) {
-				++v;
+			char v = buf[i];
+			if (isalpha(v)) {
+				g[v];
 			}
 			++i;
 		}
 	}
 
 	void calc() {
-		acorns = v-ve.count();
-		trees = v-e-acorns;
+		acorns = 0;
+		trees = 0;
+		for (graph::iterator i=g.begin();i!=g.end();++i) {
+			char v = i->first;
+			vertex& vdata = i->second;
+			if (vdata.is_visited)
+				continue;
+
+			if (vdata.adj_list.size()==0) {
+				vdata.is_visited = true;
+				++acorns;
+			} else {
+				recurse_tree(v);
+				++trees;
+			}
+		}
+	}
+
+	void recurse_tree(char v) {
+		vertex& vdata = g[v];
+		if (vdata.is_visited)
+			return;
+		vdata.is_visited = true;
+		for (vc::iterator i=vdata.adj_list.begin();i!=vdata.adj_list.end();++i) {
+			recurse_tree(*i);
+		}
 	}
 
 	void output() {
 		cout<<"There are "<<trees<<" tree(s) and "<<acorns<<" acorn(s)."<<endl;
 	}
 private:
-	int e;
-	int v;
-	bitset<26> ve;
+	graph g;
 	int acorns;
 	int trees;
 };
